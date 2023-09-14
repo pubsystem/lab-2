137f1f614a8e3c71b1496d7417b6031db1077ab3
陆宁 <luning@MacBook-Air-L.local>
2023-09-13 20:11:41 -0400

    Completes Lab 2

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = 'Ning';
+// console.log(`My first name is ${firstName}`);
+console.log('My first name is ' + firstName);
+let favNumber = 777;
+let squared = Math.pow(favNumber, 2);
+console.log(`The squared of ${favNumber} is ${squared}`);
